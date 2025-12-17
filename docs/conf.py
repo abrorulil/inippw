64bb47b079f0614c900a5359a0c66d9c72c4f9e8
ulil abror <210411100166@student.trunojoyo.ac.id>
2025-12-17 23:05:16 +0700

    Fix: Update repository URL in config

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,26 @@
+# docs/conf.py
+
 project = 'Proyek Analisis Data (inippw)'
 author = 'Abror Ulil'
 extensions = [
     'nbsphinx',
     'myst_parser',
     'sphinx.ext.mathjax',
+    'sphinx_design',
 ]
 
 html_theme = 'sphinx_book_theme'
-html_title = "Website PPW - inippw"
+html_title = "Website Analisis Data - inippw"
+
+html_theme_options = {
+    # UBAH URL DI SINI
+    "repository_url": "https://github.com/abrorulil/inippw", 
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "path_to_book": "docs",
+    "show_navbar_depth": 2,
+}
+
 html_static_path = ['_static']
-
-# Gunakan logo jika ada file pp.png di docs/_static/
-html_logo = "_static/pp.png"
-
+html_logo = "_static/pp.png" 
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
